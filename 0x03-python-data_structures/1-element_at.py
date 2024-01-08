20c347ee155918cd9d364ce05ded9c018b6d7f39
EHronek <emmanuelhronek@gmail.com>
2024-01-08 16:16:22 +0000

    pushing 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ != "__main__":
-    def element_at(my_list, idx):
-        nums = len(my_list)
-        if idx < 0 or idx > nums:
-            return None
-        else:
-            return (my_list[idx])
+def element_at(my_list, idx):
+    nums = len(my_list)
+    if idx < 0 or idx > nums:
+        return None
+    else:
+        return (my_list[idx])
