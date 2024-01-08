21189e6405478bc622d6c78b93c3caa76baeb49c
EHronek <emmanuelhronek@gmail.com>
2024-01-08 19:58:01 +0000

    pushing

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,4 @@
 def no_c(my_string):
     for c in my_string:
         if c == "c" or c == "C":
-            my_string = my_string.replace(c, '')
-        return my_string
+            return ''.join([char for char in my_string if char not in c])
