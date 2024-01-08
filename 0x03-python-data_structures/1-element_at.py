32c1740cc6ef2200f0c6db09ad2a8d08ed5d97d5
EHronek <emmanuelhronek@gmail.com>
2024-01-08 16:12:33 +0000

    task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    nums = len(my_list)
-    if idx < 0:
-        return None
-    elif idx > nums:
-        return None
-    else:
-        return (my_list[idx])
+if __name__ != "__main__":
+    def element_at(my_list, idx):
+        nums = len(my_list)
+        if idx < 0 or idx > nums:
+            return None
+        else:
+            return (my_list[idx])
