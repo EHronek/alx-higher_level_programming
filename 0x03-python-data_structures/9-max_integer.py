290f5000cb4b71a1ed713403d6c403fbc8c1ab52
EHronek <emmanuelhronek@gmail.com>
2024-01-09 17:32:49 +0000

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+    if length == 0:
+        return None
+    max_int = my_list[0]
+    for num in my_list[1:]:
+        if num > max_int:
+            max_int = num
+    return (max_int)	
