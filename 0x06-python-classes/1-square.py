49df4cc547db2069daf8afbde1eda82ced83afd6
EHronek <emmanuelhronek@gmail.com>
2024-01-27 06:36:44 +0000

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,9 +8,10 @@ class Square:
         __size (int): the size of a square
 """
     __size = None
+
     def __init__(self, value):
         """Defining the conctructor for the Square object
         Args:
             value (int): the value size of a square
 """
-        self.__size = value 
+        self.__size = value
