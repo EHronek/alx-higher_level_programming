4efdf7f20d224783d7f37ebf91829acbc1790ec0
EHronek <emmanuelhronek@gmail.com>
2024-01-27 06:34:49 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Defining a class Square that defines a square based on 0-square.py"""
+
+
+class Square:
+    """Defines a square size
+    Attributes:
+        __size (int): the size of a square
+"""
+    __size = None
+    def __init__(self, value):
+        """Defining the conctructor for the Square object
+        Args:
+            value (int): the value size of a square
+"""
+        self.__size = value 
