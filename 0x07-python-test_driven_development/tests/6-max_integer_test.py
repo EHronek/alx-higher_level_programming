bf88896fe328c39e753a144c890844108c56d940
EHronek <emmanuelhronek@gmail.com>
2024-02-02 20:30:50 +0000

    pushing task

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,6 +4,7 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """Unittests for max_integer([])"""
     def test_empty_list(self):
@@ -14,7 +15,7 @@ class TestMaxInteger(unittest.TestCase):
     def test_order_valid(self):
         """to assert if it is a valid list"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 3, 4, 2]),4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
     def test_unordered(self):
         self.assertEqual(max_integer([1, 2, 4, 3]), 4)
@@ -38,5 +39,5 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([0.0, 3.1, 3, 2.1]), 3.1)
 
     def test_raises(self):
-        self.assertRaises(TypeError, max_integer, [1, "him", 4], mesg="unorderable types: str() > int()")
-        self.assertRaises(TypeError,max_integer, [2, 4, '5'], msg="unorderable types: str() > int()")
+        self.assertRaises(TypeError, max_integer, [1, "him", 4], mesg=/"unorderable types: str() > int()")
+        self.assertRaises(TypeError, max_integer, [2, 4, '5'], msg="unorderable types: str() > int()")
