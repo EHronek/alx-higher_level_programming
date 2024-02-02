92364bd8df5341f118464d37ac1b04533b47a248
EHronek <emmanuelhronek@gmail.com>
2024-02-02 20:26:46 +0000

    pushing task

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,21 +4,20 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
 class TestMaxInteger(unittest.TestCase):
     """Unittests for max_integer([])"""
     def test_empty_list(self):
         """To test if the list is empty"""
         empty_list = []
-        self.assertEqual(max_integer(empty), None)
+        self.assertEqual(max_integer(empty_list), None)
 
     def test_order_valid(self):
         """to assert if it is a valid list"""
-        self.assertEqual(max_integer([1, 2, 3, 4], 4)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([1, 3, 4, 2]),4)
 
     def test_unordered(self):
-        self.assertEqual(max_integer(unordered), 4)
+        self.assertEqual(max_integer([1, 2, 4, 3]), 4)
 
     def test_max_beg(self):
         self.assertEqual(max_integer([4, 3, 2, 1]), 4)
@@ -36,8 +35,8 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([3, 3, 1, 2]), 3)
 
     def test_floats(self):
-        self.assetEqual(max_integer([0.0, 3.1, 3, 2.1]), 3.1)
+        self.assertEqual(max_integer([0.0, 3.1, 3, 2.1]), 3.1)
 
     def test_raises(self):
         self.assertRaises(TypeError, max_integer, [1, "him", 4], mesg="unorderable types: str() > int()")
-	self.assertRaises(TypeError,max_integer, [2, 4, '5'], msg="unorderable types: str() > int()")
+        self.assertRaises(TypeError,max_integer, [2, 4, '5'], msg="unorderable types: str() > int()")
