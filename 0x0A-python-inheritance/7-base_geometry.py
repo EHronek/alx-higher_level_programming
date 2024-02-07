b5c5a50eba2e0c0fca9e2a2616d4b442790469ee
EHronek <emmanuelhronek@gmail.com>
2024-02-07 13:48:54 +0000

    pushing task

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module defines an based on 6-base_geometry
+"""
+
+
+class BaseGeometry:
+    """ a class based of 5-base_geometry"""
+
+    def area(self):
+        """a public instance method that raise Exception
+"""
+        raise Exception("area() is not implemented")
+
+
+    def integer_validator(self, name, value):
+        """ validates ``value`` if value is not integer raise TypeError
+        if value is less or equal to 0, raise valueError exception
+        Args:
+            name (str): name argument
+            value (any): must be an integer
+"""
+        if type(value) != int:
+            raise TypeError(f"{name}  must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
