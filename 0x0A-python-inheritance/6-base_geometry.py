6acff3752ffa52d87d421708106b3519d76c1541
EHronek <emmanuelhronek@gmail.com>
2024-02-07 11:07:54 +0000

    pushing tasks

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines an based on 5-base_geometry
+"""
+
+
+class BaseGeometry:
+    """ a class based of 5-base_geometry"""
+
+    def area(self):
+        """a public instance method that raise Exception
+"""
+        raise Exception("area() is not implemented")
