b59fedd6523d4a248201dea885c2648084dd2e49
EHronek <emmanuelhronek@gmail.com>
2024-02-08 07:47:46 +0000

    pushing tasks to git

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" defines a function that that reads a text file(utf-8) and prints
+it to stdout
+"""
+def read_file(filename=''):
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_file = f.read()
+    print(read_file)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+This readme will tell more about the project python input output
