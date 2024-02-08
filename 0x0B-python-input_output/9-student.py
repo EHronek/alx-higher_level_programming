7d5249a4b2744174a38ad554f4e1e44a296f663e
EHronek <emmanuelhronek@gmail.com>
2024-02-08 19:31:53 +0000

    pushing tasks

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,7 +7,12 @@ import json
 class Student:
     """ defines a student"""
     def __init__(self, first_name, last_name, age):
-        """initialization of student """
+        """initialization of student
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
