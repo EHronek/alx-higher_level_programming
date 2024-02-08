4b40f13833d02b2c5b5277484bc21ed1b5982688
EHronek <emmanuelhronek@gmail.com>
2024-02-08 08:20:03 +0000

    pushing task

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines a functions that writes a string to a text file and
+returns the number of characters written"""
+def write_file(filename='', text=''):
+    """writes a string to a text file and returns the number of
+    number of chars written
+"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        chars_written = f.write(text)
+    return chars_written
