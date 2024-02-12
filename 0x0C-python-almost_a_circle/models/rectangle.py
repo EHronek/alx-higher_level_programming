de9ff245269e9b7e723cd9fe387e60ae53cd5f1b
EHronek <emmanuelhronek@gmail.com>
2024-02-12 17:44:36 +0000

    added area to the rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,3 +80,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """ calculates then retuns area of the rectangle"""
+        return self.height * self.width
