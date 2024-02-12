9fdab8c837fb3e6dad48e99c048e355960d55d08
EHronek <emmanuelhronek@gmail.com>
2024-02-12 20:01:18 +0000

    8.update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,23 @@ class Rectangle(Base):
     def __str__(self):
        """ for readable user friendly representation of an object"""
        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+    def update(self, *args):
+        """ it updates the rectangle with the values in the variable args"""
+        if args and len(args) != 0:
+            i = 0
+        for arg in args:
+            if i == 0:
+                if arg is None:
+                    self.__init__(self.width, self.height, self.x, self.y)
+                else:
+                    self.id = arg
+            elif i == 1:
+                self.width = arg
+            elif i == 2:
+                self.height = arg
+            elif i == 3:
+                self.x = arg
+            elif i == 4:
+                self.y == arg
+            i += 1
