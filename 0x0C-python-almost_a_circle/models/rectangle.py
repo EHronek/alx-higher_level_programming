200304413c5cdae58f2e05199852cf6239ca78e5
EHronek <emmanuelhronek@gmail.com>
2024-02-12 16:56:11 +0000

    pushing tasks to git

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Defines a class ``Rectangle`` that inherits from the ``Base`` class"""
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -19,7 +20,47 @@ class Rectangle(Base):
             y (int): the y
 """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """getter method for our rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter for the width of the rectangle """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ setter for the height of the rectangle """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for the height of the rectangle """
+        self.__height = value
+
+    @property
+    def x(self):
+        """getter for the x coodinates of the rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter method for the x coodinates of rectangle"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """ getter method for the y coodinates """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter method for the y coodinates of the rectangle"""
+        self.__y = value
