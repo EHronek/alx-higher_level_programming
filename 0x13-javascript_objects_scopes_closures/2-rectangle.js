108f67631cd7eb94571aae0125a4cb84805c950c
EHronek <emmanuelhronek@gmail.com>
2024-04-21 18:25:54 +0000

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0) w = '';
-    if (h <= 0) h = '';
+    if (w <= 0) return {};
+    if (h <= 0) return {};
     this.width = w;
     this.height = h;
   }
