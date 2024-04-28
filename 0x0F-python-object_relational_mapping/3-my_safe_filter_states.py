5100abb5e3599005701496ac96efe7cc38593e93
EHronek <emmanuelhronek@gmail.com>
2024-04-28 14:58:10 +0000

    pushing task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,10 +6,8 @@ that is safe from sql injection
 """
 if __name__ == '__main__':
     db_conn = MySQLdb.connect(
-        host='localhost',
         user=sys.argv[1],
         passwd=sys.argv[2],
-        port=3306,
         db=sys.argv[3]
     )
     cursor = db_conn.cursor()
