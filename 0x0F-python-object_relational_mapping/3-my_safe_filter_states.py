ce7e139e105c90983ab16b3fc9839718ef7327cd
EHronek <emmanuelhronek@gmail.com>
2024-04-28 15:09:28 +0000

    pushing task

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,8 +4,17 @@ import MySQLdb
 """Displays all the values in the states tab;e of the database and
 that is safe from sql injection
 """
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+if __name__ == '__main__':
+    db_conn = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    cursor = db_conn.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id"
+    if sys.argv[4] != 'Arizona' or sys.argv[4]:
+        print('')
+    cursor.execute(query, (sys.argv[4],))
+    for row in cursor.fetchall():
+        if row[1] == sys.argv[4]:
+            print(row)
