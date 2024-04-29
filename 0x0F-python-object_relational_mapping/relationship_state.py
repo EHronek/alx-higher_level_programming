c4bacbe97bbf1189c2a0ede6bdd0908cfa43941e
EHronek <emmanuelhronek@gmail.com>
2024-04-29 09:10:39 +0000

    task 100

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -12,7 +12,7 @@ from relationship_city import Base, City
 class State(Base):
     """ Maps with the states table in the database"""
     __tablename__ = 'states'
-    id  = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
 
     cities = relationship("City", backref='state', cascade='all, delete')
