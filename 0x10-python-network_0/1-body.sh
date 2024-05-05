0a27d89c323d336f6f44d683e7b69b6f3603024d
EHronek <emmanuelhronek@gmail.com>
2024-05-05 15:34:33 +0000

    task 1, 2

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Takes in a url and sends a GET request and displays body of the response
+curl -sfL "$1"
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sends DELETE request to the URL passed and displays body of response
+curl -sX DELETE "$1"
