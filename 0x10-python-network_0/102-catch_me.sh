c15ca7f782aa6f0376f9e950617c64161f350e04
EHronek <emmanuelhronek@gmail.com>
2024-05-05 16:27:10 +0000

    advanced task

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to URL that causes server to respond with message containing "You got me!"
-curl -sL --request PUT 0.0.0.0:5000/catch_me --data "user_id=98" -H "Origin:You got me!"
+curl -sL --request PUT 0.0.0.0:5000/catch_me --data "user_id=98" -H "You got me!"
