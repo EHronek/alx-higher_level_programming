0b3b1f4693e2f8e8725c8ba80ec3e78d09c55236
EHronek <emmanuelhronek@gmail.com>
2024-05-14 15:20:55 +0000

    task 16 fix

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- script that lists all shows and all genres linked to that show 
 -- if show doesn't have genre display NULL in the genre column
-SELECT tv_shows.title, tv_genres.name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genre ON tv_genres.id = tv_show_genres.genre_id ORDER BY tv_shows.title,  tv_genres.name ASC;
+SELECT tv_shows.title, tv_genres.name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id ORDER BY tv_shows.title, tv_genres.name ASC;
