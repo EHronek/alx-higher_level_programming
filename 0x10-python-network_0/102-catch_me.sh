094e295680593c7f0972a4ba85594b460c9425e0
EHronek <emmanuelhronek@gmail.com>
2025-01-17 15:22:25 +0300

    102_catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to URL that causes server to respond with message containing "You got me!
-curl "0.0.0.0:5000/catch_me" -sLX PUT -d "user_id=98"
+curl "0.0.0.0:5000/catch_me" -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98"
